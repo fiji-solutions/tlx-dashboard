5226748c5f03b35702f37f8d8995824ef3b4d76d
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-09-29 18:56:51 +0200

    Jupiter POC

---
+++ src/Jupiter.js
@@ -0,0 +1,179 @@
+import React, { useEffect, useState } from 'react';
+import CryptoChart from './components/CryptoChart';
+import {
+    Button,
+    Checkbox,
+    CircularProgress,
+    FormControl, Grid,
+    InputLabel, ListItemText,
+    MenuItem,
+    OutlinedInput,
+    Select, Tab, Tabs
+} from "@mui/material";
+import ViewAgendaOutlinedIcon from "@mui/icons-material/ViewAgendaOutlined";
+import ViewQuiltOutlinedIcon from "@mui/icons-material/ViewQuiltOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const Jupiter = () => {
+    const [assets, setAssets] = useState([]);
+    const [selectedAssets, setSelectedAssets] = useState([]);
+    const [chartData, setChartData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [tabValue, setTabValue] = useState('1.3');
+
+    const fetchAssets = async () => {
+        const response = await fetch('https://api.fijisolutions.net/jupiter-all');
+        const data = await response.json();
+        setAssets(data);
+    };
+
+    const fetchData = async () => {
+        setLoading(true);
+
+        // Generate colors for the charts
+        const generateColor = () => {
+            const r = Math.floor(Math.random() * 255);
+            const g = Math.floor(Math.random() * 255);
+            const b = Math.floor(Math.random() * 255);
+            return {
+                borderColor: `rgba(${r}, ${g}, ${b}, 1)`,
+                backgroundColor: `rgba(${r}, ${g}, ${b}, 0.2)`,
+            };
+        };
+
+        // Fetch asset data
+        const arrayPromises = selectedAssets.map(asset =>
+            fetch(`https://api.fijisolutions.net/jupiter?ids=${encodeURIComponent(asset)}`).then(response => response.json())
+        );
+
+        // Wait for all the promises to resolve
+        const results = await Promise.all(arrayPromises);
+
+        // Combine the data into a suitable format for the chart
+        const combinedData = results.map((result, index) => {
+            const { borderColor, backgroundColor } = generateColor();
+            return {
+                label: selectedAssets[index],
+                data: result[selectedAssets[index]].map(item => ({
+                    timestamp: item.timestamp,
+                    price: item.price,
+                })),
+                borderColor,
+                backgroundColor,
+            };
+        });
+
+        setChartData(combinedData);
+        setLoading(false);
+    };
+
+    const handleTabChange = (event, newValue) => {
+        setTabValue(newValue);
+    };
+
+
+    useEffect(() => {
+        fetchAssets();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Jupiter LST</h1>
+
+            <Grid
+                container
+                direction={"column"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                spacing={1}
+            >
+                <Grid item>
+                    <FormControl sx={{m: 1, width: 300}}>
+                        <InputLabel id="asset-select-label">Select Assets</InputLabel>
+                        <Select
+                            labelId="asset-select-label"
+                            id="asset-select"
+                            multiple
+                            value={selectedAssets}
+                            onChange={(e) => setSelectedAssets(e.target.value)}
+                            input={<OutlinedInput label="Select Assets"/>}
+                            renderValue={(selected) => selected.join(', ')}
+                            MenuProps={MenuProps}
+                        >
+                            {assets.map((asset) => (
+                                <MenuItem key={asset.coingeckoId} value={asset.name}>
+                                    <Checkbox checked={selectedAssets.indexOf(asset.name) > -1}/>
+                                    <img
+                                        style={{height: "24px", marginRight: "8px"}}
+                                        src={asset.logoURI}
+                                        alt={asset.name}
+                                    />
+                                    <ListItemText primary={asset.name}/>
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid>
+
+                <Grid item>
+                    <Button onClick={fetchData} variant="contained" disabled={loading}>
+                        {loading ? <CircularProgress size={25} color={"grey"}/> : "Fetch Data"}
+                    </Button>
+                </Grid>
+            </Grid>
+
+            <Grid
+                container
+                justifyContent={"center"}
+                alignItems={"center"}
+                direction={"column"}
+            >
+                <Grid item>
+                    <h4>Change charts size</h4>
+                </Grid>
+
+                <Grid item>
+                    <Tabs value={tabValue} onChange={handleTabChange}>
+                        <Tab label={(<ViewAgendaOutlinedIcon/>)} value={"1"}/>
+                        <Tab label={(<ViewQuiltOutlinedIcon/>)} value={"1.3"}/>
+                        <Tab label={(<GridViewOutlinedIcon/>)} value={"2"}/>
+                    </Tabs>
+                </Grid>
+            </Grid>
+
+
+            {chartData.length > 0 && (
+                <>
+                    <Grid
+                        container
+                        direction={"row"}
+                        justifyContent={"space-evenly"}
+                    >
+                        <Grid
+                            item
+                            xs={11 / parseFloat(tabValue)}
+                            justifyContent="center"
+                        >
+                            <Grid item xs={12}>
+                                <CryptoChart datasets={chartData} title="Asset Prices" metric="price"/>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Jupiter;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,37 +10,52 @@ import NavTabs from './components/NavTabs';
 import CoingeckoSolMemes from "./CoingeckoSolMemes";
 import RSPS from "./RSPS";
 import TGA1 from "./TGA1";
-// import TGA2 from "./TGA2";
-// import TGA3 from "./TGA3";
-// import Login from "./components/Login";
+import Jupiter from "./Jupiter";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <Router>
-            <NavTabs/>
+const Root = () => {
+    const location = useLocation();
+
+    return (
+        <>
+            {/* Conditionally render NavTabs based on the current path */}
+            {location.pathname !== '/jupiter' && <NavTabs/>}
             <Routes>
                 <Route path="/" element={<App/>}/>
-                {/*<Route path="/login" element={<Login/>}/>*/}
                 <Route path="/solana" element={<Solana/>}/>
                 <Route path="/coingecko-sol" element={<CoingeckoSol/>}/>
                 <Route path="/coingecko-sol-memes" element={<CoingeckoSolMemes/>}/>
                 <Route path="/rsps" element={<RSPS/>}/>
                 <Route path="/liquidity" element={<TGA1/>}/>
-                {/*<Route path="/liquidity-test" element={<TGA2/>}/>*/}
-                {/*<Route path="/liquidity-correlation" element={<TGA3/>}/>*/}
+                <Route path="/jupiter" element={<Jupiter/>}/>
             </Routes>
 
-            <hr/>
+            {location.pathname !== '/jupiter' && (
+                <>
+                    <hr/>
+                    <p style={{marginLeft: "8px"}}>
+                        Huge thanks to everyone who helped make this website happen:
+                        Professor Adam, JayWolf, Cedric, Calypso, Cryptosaurus Max ₿, OG_Simba
+                        —
+                    </p>
+                    <p style={{marginLeft: "8px"}}>
+                        You can view my source code here: <a
+                        href="https://github.com/fiji-solutions">https://github.com/fiji-solutions</a>
+                    </p>
+                    <p style={{marginLeft: "8px"}}>
+                        If you have any feedback or ideas on how to extend the website, tag me in TRW:
+                        @01HK0BGJQMWXQC26SRG2W46TET
+                    </p>
+                </>
+            )}
+        </>
+    );
+};
 
-            <p style={{marginLeft: "8px"}}>Huge thanks to everyone who helped make this website happen:
-                Professor Adam, JayWolf, Cedric, Calypso, Cryptosaurus Max ₿, OG_Simba
-                — </p>
-            <p style={{marginLeft: "8px"}}>You can view my source code here: <a
-                href="https://github.com/fiji-solutions">https://github.com/fiji-solutions</a></p>
-            <p style={{marginLeft: "8px"}}>If you have any feedback or ideas on how to extend the website, tag me in
-                TRW:
-                @01HK0BGJQMWXQC26SRG2W46TET</p>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Router>
+            <Root/>
         </Router>
     </React.StrictMode>
 );
