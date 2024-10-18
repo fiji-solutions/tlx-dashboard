330cf4e9012577f7a5528bca7c6c814be00aadb5
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-10-18 12:25:19 +0200

    Referral

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs, Tab, Box } from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
+import LinkComp from '@mui/material/Link';
 
 const NavTabs = () => {
     const location = useLocation();
@@ -16,6 +17,7 @@ const NavTabs = () => {
                 <Tab label="TLX & Toros" value="/" component={Link} to="/" />
                 <Tab label="Liquidity" value="/liquidity" component={Link} to="/liquidity" />
                 <Tab label="Liquidity Valuation Indicators" value="/liquidity-valuation-indicators" component={Link} to="/liquidity-valuation-indicators" />
+                <p><LinkComp href={"https://jointherealworld.com/?a=rz8pp6ccdq"} target={"_blank"}>Join the real world</LinkComp></p>
             </Tabs>
         </Box>
     );
