7eb350cd6b44f6ff73e6d94becd9169c2a646897
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-10-18 12:38:48 +0200

    Referral

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -17,7 +17,7 @@ const NavTabs = () => {
                 <Tab label="TLX & Toros" value="/" component={Link} to="/" />
                 <Tab label="Liquidity" value="/liquidity" component={Link} to="/liquidity" />
                 <Tab label="Liquidity Valuation Indicators" value="/liquidity-valuation-indicators" component={Link} to="/liquidity-valuation-indicators" />
-                <p>Join <LinkComp href={"https://jointherealworld.com/?a=rz8pp6ccdq"} target={"_blank"}>The Real World</LinkComp> before you miss out on the next Crypto Bull Run!</p>
+                <p style={{marginLeft: "8px"}}>Join <LinkComp href={"https://jointherealworld.com/?a=rz8pp6ccdq"} target={"_blank"}>The Real World</LinkComp> before you miss out on the next Crypto Bull Run!</p>
             </Tabs>
         </Box>
     );
