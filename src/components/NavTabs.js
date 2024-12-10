eb09085d7da40723355507f4e2711f8b00e966f5
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-12-10 13:40:14 +0100

    Added fiji

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Tabs, Tab, Box } from '@mui/material';
+import {Tabs, Tab, Box, Grid} from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
+import "./NavTabs.css";
 
 const NavTabs = () => {
     const location = useLocation();
@@ -11,13 +12,35 @@ const NavTabs = () => {
     };
 
     return (
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <Tabs value={value} onChange={handleChange} aria-label="navigation tabs">
-                <Tab label="TLX & Toros" value="/" component={Link} to="/" />
-                <Tab label="Liquidity" value="/liquidity" component={Link} to="/liquidity" />
-                <Tab label="Liquidity Valuation Indicators" value="/liquidity-valuation-indicators" component={Link} to="/liquidity-valuation-indicators" />
-            </Tabs>
-        </Box>
+        <Grid
+            container
+            alignItems={"center"}
+            justifyContent={"space-between"}
+        >
+            <Grid item>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="navigation tabs">
+                        <Tab label="TLX & Toros" value="/" component={Link} to="/" />
+                        <Tab label="Liquidity" value="/liquidity" component={Link} to="/liquidity" />
+                        <Tab label="Liquidity Valuation Indicators" value="/liquidity-valuation-indicators" component={Link} to="/liquidity-valuation-indicators" />
+                    </Tabs>
+                </Box>
+            </Grid>
+            <Grid
+                item
+                className="creator-section"
+                onClick={() => window.open("https://linktr.ee/fiji_solutions", "_blank")}
+            >
+                <span>Created by</span>
+                <img
+                    src="https://d2l35o8v06vi7z.cloudfront.net/fiji/fiji_xmas.png"
+                    alt="Fiji Icon"
+                    className="creator-icon"
+                />
+                <span className="creator-name">Fiji</span>
+            </Grid>
+
+        </Grid>
     );
 };
 
