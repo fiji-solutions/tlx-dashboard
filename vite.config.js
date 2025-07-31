3f25e9682933ae638e7be4fc3b8b4b58aaa9c4c7
cmoutafidis <charismoutafidis@gmail.com>
2025-08-01 01:27:53 +0200

    Fixed project

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          mui: ['@mui/material', '@mui/icons-material', '@mui/x-data-grid', '@mui/x-date-pickers'],
-          charts: ['chart.js', 'react-chartjs-2', 'chartjs-adapter-date-fns']
+    plugins: [react()],
+    esbuild: {
+        loader: 'jsx',
+        include: /src\/.*\.jsx?$/,
+        exclude: [],
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom'],
+                    mui: ['@mui/material', '@mui/icons-material', '@mui/x-data-grid', '@mui/x-date-pickers'],
+                    charts: ['chart.js', 'react-chartjs-2', 'chartjs-adapter-date-fns']
+                }
+            }
         }
-      }
+    },
+    server: {
+        port: 3000,
+        open: true
+    },
+    preview: {
+        port: 3000
     }
-  },
-  server: {
-    port: 3000,
-    open: true
-  },
-  preview: {
-    port: 3000
-  }
 })
